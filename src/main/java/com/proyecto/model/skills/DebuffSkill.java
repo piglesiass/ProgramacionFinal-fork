863866a2b5f0e75fca00a4da5df75df72ce500b7
Pablo <piglesan@gmail.com>
2026-04-25 18:27:01 +0200

    Implementacion_Debuff

---
+++ src/main/java/com/proyecto/model/skills/DebuffSkill.java
@@ -0,0 +1,33 @@
+package com.proyecto.model.skills;
+
+import com.proyecto.model.Pokemon;
+
+// Baja la defensa del enemigo
+public class DebuffSkill implements Skill {
+
+    private String name;
+    private String description;
+    private int reducirDefensa;
+
+    public DebuffSkill(String name, String description, int reducirDefensa) {
+        this.name = name;
+        this.description = description;
+        this.reducirDefensa = reducirDefensa;
+    }
+
+    @Override
+    public String getName(){ 
+        return name;
+    }
+
+    @Override
+    public String getDescription(){ 
+        return description; 
+    }
+
+    @Override
+    public String execute(Pokemon user, Pokemon target) {
+        target.reduceDefense(reducirDefensa);
+        return user.getName() + " usa " + name + "! La defensa de " + target.getName() + " baja " + reducirDefensa + ".";
+    }
+}
