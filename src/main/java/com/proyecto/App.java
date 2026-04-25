f5d0415282e82d95599d6cba22021dd626c2a8df
javier <jriveradelgado3@gmail.com>
2026-04-25 17:43:59 +0200

    implementacion app y pom

--- src/main/java/com/proyecto/App.java
+++ src/main/java/com/proyecto/App.java
@@ -8,19 +8,19 @@ import java.io.IOException;
 
 /**
  * JavaFX App
+ * @author javi
  */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
         stage.setResizable(false);
-        stage.getIcons().add(new Image(App.class.getResource("images/logo.png").toExternalForm()));
+        stage.setTitle("Juego de Combates");
         SceneManager sm = SceneManager.getInstance();
-        sm.init(stage, "views", 0.5, 0.7, "styles");
-        sm.addScene("welcome", "welcomeView");
-        sm.addScene("task", "taskView");
-
-        sm.loadScene("welcome");
+        sm.init(stage, "views", 0.5, 0.75, "styles");
+        sm.addScene("menu", "menuView");
+        sm.addScene("combat", "combatView");
+        sm.loadScene("menu");
     }
 
     public static void main(String[] args) {
